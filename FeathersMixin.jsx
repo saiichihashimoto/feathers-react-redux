975dc88dead693b506d0d24c91070afa43826b06
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-01-20 11:48:13 -0800

    FeathersMixin for react

---
+++ FeathersMixin.jsx
@@ -0,0 +1,67 @@
+var _         = require('underscore');
+var pluralize = require('pluralize');
+
+var DEFAULT_OPTIONS = { server_load: true, client_load: false, realtime: false };
+
+var actions;
+var app;
+
+module.exports = {
+	setFeathersApp: function(new_app) {
+		app = new_app;
+	},
+	setFeathersActions: function(new_actions) {
+		actions = new_actions;
+	},
+	feathers: function(resource, options) {
+		options = _.chain(options || {})
+			.defaults(DEFAULT_OPTIONS)
+			.each(function(value, name) {
+				if (!value) {
+					return;
+				}
+				this[name] = _.union(this[name], [resource]);
+			}.bind(this))
+			.value();
+
+		if (!process.browser && options.server_load) {
+			var resources = pluralize(resource);
+			var Resources = resources.charAt(0).toUpperCase() + resources.slice(1);
+
+			this.props.dispatch({
+				type:    'SERVER_ACTION',
+				payload: {
+					type: 'load' + Resources
+				}
+			});
+		}
+	},
+	// There's pretty much no reason for anything to be in componentWillMount, since feathers() can only be called AFTER it. Anything that would be there should be in feathers().
+	componentDidMount: function() {
+		_.each(this.client_load, function(resource) {
+			var resources = pluralize(resource);
+			var Resources = resources.charAt(0).toUpperCase() + resources.slice(1);
+
+			this.props.dispatch(actions['load' + Resources]());
+		}.bind(this));
+
+		_.each(this.realtime, function(resource) {
+			var Resource  = resource.charAt(0).toUpperCase() + resource.slice(1);
+			var resources = pluralize(resource);
+
+			this.cleanups = _.chain(['created', 'updated', 'patched', 'removed'])
+				.map(function(action) {
+					var dispatchAction = _.compose(this.props.dispatch, actions[action + Resource]);
+					app.service('/api/' + resources).on(action, dispatchAction);
+					return function() {
+						app.service('/api/' + resources).off(action, dispatchAction);
+					}.bind(this);
+				}.bind(this))
+				.union(this.cleanups)
+				.value();
+		}.bind(this));
+	},
+	componentWillUnmount: function() {
+		_.each(this.cleanups, _.partial);
+	}
+};
