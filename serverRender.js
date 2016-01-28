a3145f68aeb0777109829c0565c5446a41f6023d
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-01-27 23:29:14 -0800

    return a promise instead of a using a callback for serverRender

--- serverRender.js
+++ serverRender.js
@@ -1,45 +1,24 @@
 var _        = require('underscore');
-var async    = require('async');
 var ReactDOM = require('react-dom/server');
 
-module.exports = function(element, store, actions, callback) {
-	var markup;
-	var done_server_actions = [];
+module.exports = function serverRender(element, store, actions, done_server_actions) {
+	done_server_actions = done_server_actions || [];
 
-	async.until(
-		function() {
-			markup = ReactDOM.renderToString(element);
-			return _.chain(store)
-				.result('getState')
-				.result('server_actions')
-				.difference(done_server_actions)
-				.isEmpty()
-				.value();
-		},
-		function(callback) {
-			var todo_actions = _.chain(store)
-				.result('getState')
-				.result('server_actions')
-				.without(done_server_actions)
-				.value();
-			async.each(
-				todo_actions,
-				function(action, callback) {
-					store.dispatch(actions[action.type](actions.params)).then(
-						function() {
-							callback();
-						},
-						callback
-					);
-				},
-				function(err) {
-					done_server_actions = _.union(done_server_actions, todo_actions);
-					callback(err);
-				}
-			);
-		},
-		function(err) {
-			callback(err, markup);
-		}
-	);
+	var markup            = ReactDOM.renderToString(element);
+	var remaining_actions = _.chain(store)
+		.result('getState')
+		.result('server_actions')
+		.difference(done_server_actions)
+		.value();
+
+	if (_.isEmpty(remaining_actions)) {
+		return Promise.resolve(markup);
+	}
+
+	return Promise.all(remaining_actions.map(function(action) {
+		return store.dispatch(actions[action.type](actions.params));
+	}))
+	.then(function() {
+		return serverRender(element, store, actions, _.union(done_server_actions, remaining_actions));
+	});
 };
