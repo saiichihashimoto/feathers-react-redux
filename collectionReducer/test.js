7dd67da3107d34ffe8f17c7434b8308b908212e5
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-03-17 22:09:30 -0700

    some quick restructuring of the test

--- collectionReducer/test.js
+++ collectionReducer/test.js
@@ -9,89 +9,91 @@ describe('collectionReducer', function() {
 		collectionReducer = require('.');
 	});
 
-	it('should populate state on LOAD_<RESOURCES>', function() {
-		var initialState = {
-			data: {
-				0: {
-					data: {
-						id:    0,
-						value: 'zero'
-					}
-				}
-			}
-		};
-		var state = collectionReducer('resource')(initialState, {
-			type:    'LOAD_RESOURCES',
-			payload: [
-				{
-					id:    1,
-					value: 'one'
-				},
-				{
-					id:    2,
-					value: 'two'
-				}
-			]
-		});
-
-		expect(state).to.not.equal(initialState);
-		expect(state)
-			.to.have.property('data')
-				.not.equal(initialState.data);
-		expect(state)
-			.to.have.property('data')
-				.deep.equal({
+	describe('on LOAD_<RESOURCES>', function() {
+		it('should populate state', function() {
+			var initialState = {
+				data: {
 					0: {
 						data: {
 							id:    0,
 							value: 'zero'
 						}
-					},
-					1: {
-						data: {
-							id:    1,
-							value: 'one'
-						}
-					},
-					2: {
-						data: {
-							id:    2,
-							value: 'two'
-						}
 					}
-				});
-	});
+				}
+			};
+			var state = collectionReducer('resource')(initialState, {
+				type:    'LOAD_RESOURCES',
+				payload: [
+					{
+						id:    1,
+						value: 'one'
+					},
+					{
+						id:    2,
+						value: 'two'
+					}
+				]
+			});
 
-	it('should overwrite a resource on LOAD_<RESOURCES>', function() {
-		var initialState = {
-			data: {
-				0: {
-					data: {
-						id:    0,
-						value: 'zero'
-					}
-				}
-			}
-		};
-		var state = collectionReducer('resource')(initialState, {
-			type:    'LOAD_RESOURCES',
-			payload: [
-				{
-					id:    0,
-					value: 'zero 2'
-				}
-			]
+			expect(state).to.not.equal(initialState);
+			expect(state)
+				.to.have.property('data')
+					.not.equal(initialState.data);
+			expect(state)
+				.to.have.property('data')
+					.deep.equal({
+						0: {
+							data: {
+								id:    0,
+								value: 'zero'
+							}
+						},
+						1: {
+							data: {
+								id:    1,
+								value: 'one'
+							}
+						},
+						2: {
+							data: {
+								id:    2,
+								value: 'two'
+							}
+						}
+					});
 		});
 
-		expect(state)
-			.to.have.property('data')
-				.deep.equal({
+		it('should overwrite a resources', function() {
+			var initialState = {
+				data: {
 					0: {
 						data: {
 							id:    0,
-							value: 'zero 2'
+							value: 'zero'
 						}
 					}
-				});
+				}
+			};
+			var state = collectionReducer('resource')(initialState, {
+				type:    'LOAD_RESOURCES',
+				payload: [
+					{
+						id:    0,
+						value: 'zero 2'
+					}
+				]
+			});
+
+			expect(state)
+				.to.have.property('data')
+					.deep.equal({
+						0: {
+							data: {
+								id:    0,
+								value: 'zero 2'
+							}
+						}
+					});
+		});
 	});
 });
